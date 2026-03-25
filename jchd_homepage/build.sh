6eaae0a4bb0301fbb285b2168d858147d8b13443
JCHD <jchd@JCHDnoMacBook-Air.local>
2026-03-25 18:17:11 +0900

    change build.sh

--- jchd_homepage/build.sh
+++ jchd_homepage/build.sh
@@ -11,4 +11,10 @@ pip install -r requirements.txt
 
 # Django collect static
 python manage.py collectstatic --noinput --clear
+# Migrate database
 python manage.py migrate --noinput
+# Create superuser
+if [[ $CREATE_SUPERUSER == "true" ]]; then
+    echo "Creating superuser..."
+    python manage.py createsuperuser --noinput
+fi
